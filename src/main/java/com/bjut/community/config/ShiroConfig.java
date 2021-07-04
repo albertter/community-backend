b84adcb7e362a1b55688a6fd5caab40a97a28b0c
albertter <alberdder@gmail.com>
2021-07-04 22:12:13 +0800

    更新shiro

--- src/main/java/com/bjut/community/config/ShiroConfig.java
+++ src/main/java/com/bjut/community/config/ShiroConfig.java
@@ -2,82 +2,95 @@ package com.bjut.community.config;
 
 
 import at.pollux.thymeleaf.shiro.dialect.ShiroDialect;
+import com.bjut.community.jwt.JWTFilter;
 import com.bjut.community.shiro.CustomerRealm;
-import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import com.bjut.community.shiro.SystemLogoutFilter;
 import org.apache.shiro.cache.CacheManager;
 import org.apache.shiro.cache.MemoryConstrainedCacheManager;
+import org.apache.shiro.mgt.DefaultSessionStorageEvaluator;
+import org.apache.shiro.mgt.DefaultSubjectDAO;
 import org.apache.shiro.mgt.RememberMeManager;
 import org.apache.shiro.realm.Realm;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.servlet.SimpleCookie;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
+import javax.servlet.Filter;
 import java.util.HashMap;
+import java.util.Map;
 
+/**
+ * @author zhen
+ */
 @Configuration
 public class ShiroConfig {
+    private static final Logger logger = LoggerFactory.getLogger(ShiroConfig.class);
 
     // 1. shirofilter
     @Bean(name = "shiroFilterFactoryBean")
     public ShiroFilterFactoryBean getShiroFilterFactoryBean(DefaultWebSecurityManager defaultWebSecurityManager) {
         ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+
+        // 添加自己的过滤器并且取名为jwt
+        Map<String, Filter> filterMap = new HashMap<>();
+        filterMap.put("jwt", new JWTFilter());
+        filterMap.put("logout", new SystemLogoutFilter());
+        shiroFilterFactoryBean.setFilters(filterMap);
         //给filter设置安全管理器
         shiroFilterFactoryBean.setSecurityManager(defaultWebSecurityManager);
+        shiroFilterFactoryBean.setUnauthorizedUrl("/401");
         //配置系统受限资源
         //配置系统公共资源
         HashMap<String, String> map = new HashMap<>();
-        map.put("/resources/**", "anon");
-        map.put("/login", "anon");//anon 设置为公共资源  放行资源放在下面
-        map.put("/register", "anon");//anon 设置为公共资源  放行资源放在下面
-        map.put("/captcha", "anon");//anon 设置为公共资源  放行资源放在下面
-        map.put("/index", "anon");
-        map.put("/discuss/**", "anon");
 
-        map.put("/css/**", "anon");
-        map.put("/jss/**", "anon");
-        map.put("/img/**", "anon");
+        map.put("/logout", "logout");
+        map.put("/**", "jwt");
 
-        map.put("/**", "authc");//authc 请求这个资源需要认证和授权
-
-//        shiroFilterFactoryBean.setLoginUrl("/user/loginview");
         shiroFilterFactoryBean.setFilterChainDefinitionMap(map);
 
+        logger.info("Shiro >> Shiro拦截器工厂类注入成功");
         return shiroFilterFactoryBean;
     }
 
     @Bean
     public DefaultWebSecurityManager getDefaultWebSecurityManager(Realm realm) {
         DefaultWebSecurityManager defaultWebSecurityManager = new DefaultWebSecurityManager();
+        // 设置 realm.
         defaultWebSecurityManager.setRealm(realm);
-        defaultWebSecurityManager.setRememberMeManager(rememberMeManager());
+//        defaultWebSecurityManager.setRememberMeManager(rememberMeManager());
+        /*
+         * 关闭shiro自带的session，详情见文档
+         * http://shiro.apache.org/session-management.html#SessionManagement-StatelessApplications%28Sessionless%29
+         */
+        DefaultSubjectDAO subjectDAO = new DefaultSubjectDAO();
+        DefaultSessionStorageEvaluator defaultSessionStorageEvaluator = new DefaultSessionStorageEvaluator();
+        defaultSessionStorageEvaluator.setSessionStorageEnabled(false);
+        subjectDAO.setSessionStorageEvaluator(defaultSessionStorageEvaluator);
+        defaultWebSecurityManager.setSubjectDAO(subjectDAO);
+
         return defaultWebSecurityManager;
     }
 
+    /**
+     * 自定义身份认证 realm;
+     * <p>
+     * 必须写这个类，并加上 @Bean 注解，目的是注入 CustomRealm，
+     * 否则会影响 CustomRealm类 中其他类的依赖注入
+     */
     @Bean
     public Realm getRealm() {
-        Realm realm = new CustomerRealm();
-
-//        //修改凭证校验匹配器
-        HashedCredentialsMatcher credentialsMatcher = new HashedCredentialsMatcher();
-//        //设置加密算法为md5
-//        credentialsMatcher.setHashAlgorithmName("MD5");
-//        //设置散列次数
-//        credentialsMatcher.setHashIterations(1024);
-//        realm.setCredentialsMatcher(credentialsMatcher);
-//
-//
-//        //开启缓存管理
-//        realm.setCacheManager(new RedisCacheManager());
-//        realm.setCachingEnabled(true);//开启全局缓存
-//        realm.setAuthenticationCachingEnabled(true);//认证认证缓存
-//        realm.setAuthenticationCacheName("authenticationCache");
-//        realm.setAuthorizationCachingEnabled(true);//开启授权缓存
-//        realm.setAuthorizationCacheName("authorizationCache");
-        return realm;
+        return new CustomerRealm();
     }
+
 
     @Bean
     public ShiroDialect shiroDialect() {
@@ -104,4 +117,29 @@ public class ShiroConfig {
     }
 
 
+    /**
+     * 下面的代码是添加注解支持
+     */
+    @Bean
+    @DependsOn("lifecycleBeanPostProcessor")
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
+        // 强制使用cglib，防止重复代理和可能引起代理出错的问题
+        // https://zhuanlan.zhihu.com/p/29161098
+        defaultAdvisorAutoProxyCreator.setProxyTargetClass(true);
+        return defaultAdvisorAutoProxyCreator;
+    }
+
+    @Bean
+    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+        return new LifecycleBeanPostProcessor();
+    }
+
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(DefaultWebSecurityManager securityManager) {
+        AuthorizationAttributeSourceAdvisor advisor = new AuthorizationAttributeSourceAdvisor();
+        advisor.setSecurityManager(securityManager);
+        return advisor;
+
+    }
 }
